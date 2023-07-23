d2210a569fa9d5a8249f8d9759bcdda6c5509538
RomanSimernin <roman.simernin@saritasa.com>
2023-07-23 18:16:01 +0300

    Rename to ApiKeyHeaderInterceptor JC-542

--- apps/angular/src/core/interceptors/apiKeyHeaderInterceptor.ts
+++ apps/angular/src/core/interceptors/apiKeyHeaderInterceptor.ts
@@ -4,11 +4,11 @@ import { Observable } from 'rxjs';
 
 import { environment } from '@js-camp/angular/environments/environment';
 
-/** Interceptor for adding key to request.  */
+/** Interceptor for adding header for api key to request. */
 @Injectable({
     providedIn: 'root'
 })
-export class ApiKeyInterceptor implements HttpInterceptor {
+export class ApiKeyHeaderInterceptor implements HttpInterceptor {
 
     /** @inheritdoc */
     public intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
