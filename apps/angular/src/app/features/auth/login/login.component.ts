ea6397e0a24e18e34286129188b4c818ef80eeb9
RomanSimernin <roman.simernin@saritasa.com>
2023-08-23 17:32:12 +0300

    Add sending login request JC-548

--- apps/angular/src/app/features/auth/login/login.component.ts
+++ apps/angular/src/app/features/auth/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '@js-camp/angular/core/services/auth.service';
+import { first } from 'rxjs';
 
 /** Login component. */
 @Component({
@@ -16,11 +18,29 @@ export class LoginComponent {
 	}>;
 
 	public constructor(
+		private readonly authService: AuthService,
 		formBuilder: FormBuilder,
 	) {
 		this.formGroup = formBuilder.group({
 			email: ['', [Validators.required, Validators.email]],
 			password: ['', [Validators.required]],
 		});
+	}
+
+	/** Handle login form submitting. */
+	public handleSubmit(): void {
+		if (this.formGroup.invalid) {
+			return;
+		}
+
+		const formData = this.formGroup.getRawValue();
+
+		this.authService.login({
+			email: formData.email ?? '',
+			password: formData.password ?? '',
+		}).pipe(
+			first(),
+		)
+			.subscribe();
 	}
 }
