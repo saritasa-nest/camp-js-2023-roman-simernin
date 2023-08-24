242d15a355c0fe9fb7aaa1758c7e4edee8f11f1d
RomanSimernin <roman.simernin@saritasa.com>
2023-08-24 19:29:01 +0300

    Create TokensStorageService JC-548

--- apps/angular/src/core/services/auth.service.ts
+++ apps/angular/src/core/services/auth.service.ts
@@ -3,13 +3,14 @@ import { Injectable } from '@angular/core';
 
 import { LoginModel } from '@js-camp/core/models/login.model';
 
-import { Observable, map } from 'rxjs';
+import { Observable, map, tap } from 'rxjs';
 import { TokensDto } from '@js-camp/core/dtos/tokens.dto';
 import { LoginModelMapper } from '@js-camp/core/mappers/login.model.mapper';
 import { TokensModelMapper } from '@js-camp/core/mappers/tokens.model.mapper';
 import { TokensModel } from '@js-camp/core/models/tokens.model';
 
 import { ApiUriBuilder } from './api-uri-builder';
+import { TokensStorageService } from './tokens-storage.service';
 
 /** Service for authentication. */
 @Injectable()
@@ -18,6 +19,7 @@ export class AuthService {
 	public constructor(
 		private readonly httpClient: HttpClient,
 		private readonly apiUriBuilder: ApiUriBuilder,
+		private readonly tokenStorageService: TokensStorageService,
 	) {
 
 	}
@@ -31,7 +33,8 @@ export class AuthService {
 
 		return this.httpClient.post<TokensDto>(uri, LoginModelMapper.ToDto(loginModel))
 			.pipe(
-				map(authResultDto => TokensModelMapper.fromDto(authResultDto)),
+				map(tokensDto => TokensModelMapper.fromDto(tokensDto)),
+				tap(tokens => this.tokenStorageService.save(tokens)),
 			);
 	}
 
---
+++ apps/angular/src/core/services/tokens-storage.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+import { TokensModel } from '@js-camp/core/models/tokens.model';
+
+/** Tokens storage service. */
+@Injectable({
+	providedIn: 'root',
+})
+export class TokensStorageService {
+
+	private readonly tokensKey = 'tokens';
+
+	private tokens: TokensModel | null = null;
+
+	/**
+	 * Save tokens in storage.
+	 * @param tokens Tokens.
+	 */
+	public save(tokens: TokensModel): void {
+		this.tokens = tokens;
+		localStorage.setItem(this.tokensKey, JSON.stringify(tokens));
+	}
+
+	/**
+	 * Get data from localStorage.
+	 */
+	public get(): TokensModel | null {
+		if (this.tokens !== null) {
+			return this.tokens;
+		}
+
+		const tokensAsJson = localStorage.getItem(this.tokensKey);
+
+		if (tokensAsJson === null) {
+			return null;
+		}
+
+		return JSON.parse(this.tokensKey) as TokensModel;
+	}
+
+	/**
+	 * Delete tokens from storage.
+	 */
+	public delete(): void {
+		this.tokens = null;
+		localStorage.removeItem(this.tokensKey);
+	}
+}
