aa8e7211aa551260d901c98716e9f3dab8f186bb
RomanSimernin <roman.simernin@saritasa.com>
2023-08-25 13:01:00 +0300

    Fix typos JC-543

--- apps/angular/src/core/services/anime-service.ts
+++ apps/angular/src/core/services/anime-service.ts
@@ -8,9 +8,7 @@ import { AnimeDto } from '@js-camp/core/dtos/anime.dto';
 import { PaginationDto } from '@js-camp/core/dtos/pagination.dto';
 import { Pagination } from '@js-camp/core/models/pagination';
 import { AnimeMapper } from '@js-camp/core/mappers/anime.mapper';
-
 import { AnimeParameters } from '@js-camp/core/models/anime-parameters';
-
 import { AnimeParametersMapper } from '@js-camp/core/mappers/anime-parameters.mapper';
 
 import { ApiUriBuilder } from './api-uri-builder';
