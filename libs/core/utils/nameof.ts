ac65c9b8d193aeb64b511eca7eab70c975c5fe8a
RomanSimernin <roman.simernin@saritasa.com>
2023-08-27 15:58:51 +0300

    Add obeject type constaint for nameof function JC-543

--- libs/core/utils/nameof.ts
+++ libs/core/utils/nameof.ts
@@ -2,4 +2,4 @@
  * Get function for type that return its passed field name.
  * Has compilation error if field doesn't exist.
  */
-export const nameofFactory = <T>() => (fieldName: keyof T) => fieldName;
+export const nameofFactory = <T extends object>() => (fieldName: keyof T) => fieldName;
