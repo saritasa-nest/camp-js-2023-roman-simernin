0a71184441a55514eb7b14b22523db705a1328f1
RomanSimernin <roman.simernin@saritasa.com>
2023-08-28 17:07:12 +0300

    Initialize registration component JC-548

--- apps/angular/src/app/features/auth/auth.module.ts
+++ apps/angular/src/app/features/auth/auth.module.ts
@@ -2,24 +2,19 @@ import { NgModule } from '@angular/core';
 
 import { AuthService } from '@js-camp/angular/core/services/auth.service';
 
-import { LoginComponent } from './login/login.component';
-
 import { MatInputModule } from '@angular/material/input';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { CommonModule } from '@angular/common';
 
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
+
 /** Anime table module. */
 @NgModule({
-	imports: [
-        MatInputModule,
-        FormsModule,
-        ReactiveFormsModule,
-        MatButtonModule,
-		CommonModule,
-	],
-	declarations: [LoginComponent],
+	imports: [MatInputModule, FormsModule, ReactiveFormsModule, MatButtonModule, CommonModule],
+	declarations: [LoginComponent, RegistrationComponent],
 	exports: [LoginComponent],
 	providers: [AuthService],
 })
-export class AuthModule { }
+export class AuthModule {}
