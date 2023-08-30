e32d7a8a63eec5b721ff1e27a5f61075139d7701
RomanSimernin <roman.simernin@saritasa.com>
2023-08-30 19:42:31 +0300

    Refactor toArray function in EnumUtils JC-543

--- libs/core/utils/enum.utils.ts
+++ libs/core/utils/enum.utils.ts
@@ -1,18 +1,24 @@
 export namespace EnumUtils {
 
 	/**
-		* Convert enum to string array.
-  * @param enumeration - Enumeration.
-  */
-	export function toArray<TKey extends string, TValue>(enumeration: { [key in TKey]: TValue }): readonly TValue[] {
-		return Object.values(enumeration);
+	 * Convert enum to string array.
+	 * @param enumeration - Enumeration.
+	 */
+	export function toArray<T extends Record<string, unknown>>(enumeration: T): readonly T[keyof T] [] {
+		return Object.keys(enumeration)
+			.filter(key => isNaN(Number(key)))
+			.map(key => enumeration[key])
+			.filter(
+				(val): val is T[keyof T] =>
+					typeof val === 'number' || typeof val === 'string',
+			);
 	}
 
 	/**
-		* Convert enum to string array.
-		* @param enumValueAsString - Enum value as string.
-  * @param enumeration - Enumeration.
-  */
+	 * Convert enum to string array.
+	 * @param enumValueAsString - Enum value as string.
+	 * @param enumeration - Enumeration.
+	 */
 	export function fromString<TKey extends string, TValue extends string>(
 		enumValueAsString: string,
 		enumeration: { [key in TKey]: TValue },
