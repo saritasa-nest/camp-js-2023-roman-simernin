71406cd56b63bb5215585d6458569ed0944de88a
RomanSimernin <roman.simernin@saritasa.com>
2023-08-31 19:08:33 +0300

    Remove nameof function JC-543

--- libs/core/utils/nameof.ts
+++
@@ -1,5 +0,0 @@
-/**
- * Get function for type that return its passed field name.
- * Has compilation error if field doesn't exist.
- */
-export const nameofFactory = <T extends object>() => (fieldName: keyof T) => fieldName;
