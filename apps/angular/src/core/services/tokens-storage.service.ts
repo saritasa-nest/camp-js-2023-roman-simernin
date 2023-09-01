8ae30b1df709f6ea5189a6ee2b7e0abf0d84fb86
RomanSimernin <roman.simernin@saritasa.com>
2023-09-01 11:14:34 +0300

    Remove token caching JC-548

--- apps/angular/src/core/services/tokens-storage.service.ts
+++ apps/angular/src/core/services/tokens-storage.service.ts
@@ -9,37 +9,27 @@ export class TokensStorageService {
 
 	private readonly tokensKey = 'tokens';
 
-	private tokens: TokensModel | null = null;
-
 	/**
 	 * Save tokens in storage.
 	 * @param tokens Tokens.
 	 */
 	public save(tokens: TokensModel): void {
-		this.tokens = tokens;
 		localStorage.setItem(this.tokensKey, JSON.stringify(tokens));
 	}
 
 	/** Get data from localStorage. */
 	public get(): TokensModel | null {
-		if (this.tokens !== null) {
-			return this.tokens;
-		}
-
 		const tokensAsJson = localStorage.getItem(this.tokensKey);
 
 		if (tokensAsJson === null) {
 			return null;
 		}
 
-		this.tokens = JSON.parse(tokensAsJson) as TokensModel;
-
-		return this.tokens;
+		return JSON.parse(tokensAsJson) as TokensModel;
 	}
 
 	/** Delete tokens from storage. */
 	public delete(): void {
-		this.tokens = null;
 		localStorage.removeItem(this.tokensKey);
 	}
 }
