4506c3a5c278aeb585d9c926548fc6a338f68b45
RomanSimernin <roman.simernin@saritasa.com>
2023-09-08 13:02:42 +0300

    Add type for httpInterceptorProvider JC-548

--- apps/angular/src/core/interceptors/http-interceptor-provider.ts
+++ apps/angular/src/core/interceptors/http-interceptor-provider.ts
@@ -1,10 +1,11 @@
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { Provider } from '@angular/core';
 
 import { ApiKeyHeaderInterceptor } from './api-key-header.interceptor';
 import { AuthenticationInterceptor } from './authentication.interceptor';
 
 /** Http interceptors in outside-in order. */
-export const httpInterceptorProvider = [
+export const httpInterceptorProvider: Provider[] = [
 	{ provide: HTTP_INTERCEPTORS, useClass: ApiKeyHeaderInterceptor, multi: true },
 	{ provide: HTTP_INTERCEPTORS, useClass: AuthenticationInterceptor, multi: true },
 ];
