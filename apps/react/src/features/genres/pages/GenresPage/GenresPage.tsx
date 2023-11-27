aeef6e849aeffce88434d131e05bacfdc31e3e0d
RomanSimernin <roman.simernin@saritasa.com>
2023-11-27 12:11:49 +0300

    Add loader to genres page JC-588

--- apps/react/src/features/genres/pages/GenresPage/GenresPage.tsx
+++ apps/react/src/features/genres/pages/GenresPage/GenresPage.tsx
@@ -2,6 +2,7 @@ import { memo, useEffect, FC } from 'react';
 import { fetchGenres } from '@js-camp/react/store/genre/dispatchers';
 import { selectGenres, selectAreGenresLoading } from '@js-camp/react/store/genre/selectors';
 import { useAppDispatch, useAppSelector } from '@js-camp/react/store';
+import { Loader } from '@js-camp/react/components/Loader/Loader';
 
 import { GenreCard } from '../../components/GenreCard';
 
@@ -15,15 +16,11 @@ const GenresPageComponent: FC = () => {
 		dispatch(fetchGenres());
 	}, [dispatch]);
 
-	if (isLoading) {
-		return <div>Loading</div>;
-	}
-
 	return (
-		<>
+		<Loader isLoading={isLoading}>
 			<h1>Genres</h1>
 			{genres.map(genre => <GenreCard key={genre.id} genre={genre} />)}
-		</>
+		</Loader>
 	);
 };
 
