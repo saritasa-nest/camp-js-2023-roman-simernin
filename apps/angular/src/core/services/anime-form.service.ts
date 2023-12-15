245027eb9dfa555cffa6cded35e006d1e2b05946
RomanSimernin <roman.simernin@saritasa.com>
2023-12-15 13:32:37 +0300

    Refactor adding new genres and studios JC-568

--- apps/angular/src/core/services/anime-form.service.ts
+++ apps/angular/src/core/services/anime-form.service.ts
@@ -5,14 +5,13 @@ import { StudioService } from '@js-camp/angular/core/services/studio.service';
 import { AnimeFormData } from '@js-camp/core/models/anime/anime-form-data';
 import { ImageFileType } from '@js-camp/core/models/s3/image-file-type';
 import { AnimeFormDataMapper } from '@js-camp/core/mappers/anime/anime-form-data.mapper';
-import { Observable, combineLatest, from, map, mergeMap, of, switchMap, toArray } from 'rxjs';
+import { Observable, combineLatest, forkJoin, from, map, mergeMap, of, switchMap, toArray } from 'rxjs';
 
 import { AnimeService } from './anime-service';
 
 /** Anime form service. */
 @Injectable()
 export class AnimeFormService {
-
 	private readonly genreService = inject(GenreService);
 
 	private readonly studioService = inject(StudioService);
@@ -26,9 +25,9 @@ export class AnimeFormService {
 	 * @param id - Anime id.
 	 */
 	public getAnimeFormData(id: number): Observable<AnimeFormData> {
-		return this.animeService.getAnimeById(id).pipe(
-			map(animeDetails => AnimeFormDataMapper.fromDetails(animeDetails)),
-		);
+		return this.animeService
+			.getAnimeById(id)
+			.pipe(map((animeDetails) => AnimeFormDataMapper.fromDetails(animeDetails)));
 	}
 
 	/**
@@ -42,12 +41,8 @@ export class AnimeFormService {
 			this.addImageToFileStorage(formData),
 		]).pipe(
 			switchMap(([genreIds, studioIds, imageUrl]) =>
-				this.animeService.createAnime(AnimeFormDataMapper.toCreateData(
-					formData,
-					genreIds,
-					studioIds,
-					imageUrl,
-				))),
+				this.animeService.createAnime(AnimeFormDataMapper.toCreateData(formData, genreIds, studioIds, imageUrl))
+			)
 		);
 	}
 
@@ -63,12 +58,8 @@ export class AnimeFormService {
 			this.addImageToFileStorage(formData),
 		]).pipe(
 			switchMap(([genreIds, studioIds, imageUrl]) =>
-				this.animeService.editAnime(id, AnimeFormDataMapper.toEditData(
-					formData,
-					genreIds,
-					studioIds,
-					imageUrl,
-				))),
+				this.animeService.editAnime(id, AnimeFormDataMapper.toEditData(formData, genreIds, studioIds, imageUrl))
+			)
 		);
 	}
 
@@ -79,21 +70,20 @@ export class AnimeFormService {
 	 */
 	private createGenres(formData: AnimeFormData): Observable<number[]> {
 		const existingGenresIds = formData.genres
-			.map(genre => genre.id)
+			.map((genre) => genre.id)
 			.filter((genreId): genreId is number => genreId !== null);
 
-		const genresToCreate = formData.genres
-			.filter(genre => genre.id === null);
+		const genresToCreate = formData.genres.filter((genre) => genre.id === null);
 
 		if (genresToCreate.length === 0) {
 			return of(existingGenresIds);
 		}
 
-		return from(genresToCreate).pipe(
-			mergeMap(genreToCreate => this.genreService.createGenre(genreToCreate.name)),
-			toArray(),
-			map(createdGenreIds => createdGenreIds.concat(existingGenresIds)),
+		const genresToCreateRequests = genresToCreate.map((genreToCreate) =>
+			this.genreService.createGenre(genreToCreate.name)
 		);
+
+		return forkJoin(genresToCreateRequests).pipe(map((createdGenreIds) => createdGenreIds.concat(existingGenresIds)));
 	}
 
 	/**
@@ -103,20 +93,21 @@ export class AnimeFormService {
 	 */
 	private createStudios(formData: AnimeFormData): Observable<number[]> {
 		const existingStudioIds = formData.studios
-			.map(studio => studio.id)
+			.map((studio) => studio.id)
 			.filter((studio): studio is number => studio !== null);
 
-		const studiosToCreate = formData.studios
-			.filter(studio => studio.id === null);
+		const studiosToCreate = formData.studios.filter((studio) => studio.id === null);
 
 		if (studiosToCreate.length === 0) {
 			return of(existingStudioIds);
 		}
 
-		return from(studiosToCreate).pipe(
-			mergeMap(studioToCreate => this.studioService.createStudio(studioToCreate.name)),
-			toArray(),
-			map(createdStudioIds => createdStudioIds.concat(existingStudioIds)),
+		const studiosToCreateRequests = studiosToCreate.map((studioToCreate) =>
+			this.studioService.createStudio(studioToCreate.name)
+		);
+
+		return forkJoin(studiosToCreateRequests).pipe(
+			map((createdStudioIds) => createdStudioIds.concat(existingStudioIds))
 		);
 	}
 
